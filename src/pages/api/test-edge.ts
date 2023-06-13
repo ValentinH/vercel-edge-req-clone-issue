fbea221ffc032a7262827af6c127eb7c4380bf9a
Valentin Hervieu <valentin@hervi.eu>
2023-06-13 11:25:03 +0200

    first commit

---
+++ src/pages/api/test-edge.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+  runtime: 'edge',
+  regions: ['fra1'],
+};
+
+export default async function handler(req: NextRequest) {
+  const payload = await req.clone().json();
+  const payload2 = await req.json();
+
+  return NextResponse.json({ payload, payload2 });
+}
